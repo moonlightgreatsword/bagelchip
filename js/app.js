23bb9d929ba2f0bae38625190911ad4e2f494910
msuh <suh.mikep@gmail.com>
2022-07-14 15:48:32 -0500

    add wireframes

---
+++ js/app.js
@@ -0,0 +1,81 @@
+// define buttons
+
+const a1 = document.getElementById("A1")
+const b1 = document.getElementById("B1")
+const c1 = document.getElementById("C1")
+const d1 = document.getElementById("D1")
+const e1 = document.getElementById("E1")
+const f1 = document.getElementById("F1")
+const a2 = document.getElementById("A2")
+const b2 = document.getElementById("B2")
+const c2 = document.getElementById("C2")
+const d2 = document.getElementById("D2")
+const e2 = document.getElementById("E2")
+const f2 = document.getElementById("F2")
+const a3 = document.getElementById("A3")
+const b3 = document.getElementById("B3")
+const c3 = document.getElementById("C3")
+const d3 = document.getElementById("D3")
+const e3 = document.getElementById("E3")
+const f3 = document.getElementById("F3")
+const a4 = document.getElementById("A4")
+const b4 = document.getElementById("B4")
+const c4 = document.getElementById("C4")
+const d4 = document.getElementById("D4")
+const e4 = document.getElementById("E4")
+const f4 = document.getElementById("F4")
+const a5 = document.getElementById("A5")
+const b5 = document.getElementById("B5")
+const c5 = document.getElementById("C5")
+const d5 = document.getElementById("D5")
+const e5 = document.getElementById("E5")
+const f5 = document.getElementById("F5")
+const a6 = document.getElementById("A6")
+const b6 = document.getElementById("B6")
+const c6 = document.getElementById("C6")
+const d6 = document.getElementById("D6")
+const e6 = document.getElementById("E6")
+const f6 = document.getElementById("F6")
+const a7 = document.getElementById("A7")
+const b7 = document.getElementById("B7")
+const c7 = document.getElementById("C7")
+const d7 = document.getElementById("D7")
+const e7 = document.getElementById("E7")
+const f7 = document.getElementById("F7")
+const a8 = document.getElementById("A8")
+const b8 = document.getElementById("B8")
+const c8 = document.getElementById("C8")
+const d8 = document.getElementById("D8")
+const e8 = document.getElementById("E8")
+const f8 = document.getElementById("F8")
+const a9 = document.getElementById("A9")
+const b9 = document.getElementById("B9")
+const c9 = document.getElementById("C9")
+const d9 = document.getElementById("D9")
+const e9 = document.getElementById("E9")
+const f9 = document.getElementById("F9")
+const a10 = document.getElementById("A10")
+const b10 = document.getElementById("B10")
+const c10 = document.getElementById("C10")
+const d10 = document.getElementById("D10")
+const e10 = document.getElementById("E10")
+const f10 = document.getElementById("F10")
+
+// create class to define players
+
+// class Player {
+//     constructor () {
+//         this.name = "";
+//     }
+//     namePlayer (name) {
+//         this.name = `${name}`;
+//     }
+// }
+
+// class firstPlayer extends Player {
+
+// }
+
+// class secondPlayer extends Player {
+
+// }
